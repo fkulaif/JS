846ac14e8027595c96682e0c0dcfdd1f789c9d71
Pingosoft <fernandokulaif@gmail.com>
2021-05-22 04:32:42 -0400

    index.html

--- js/script.js
+++ js/script.js
@@ -1,29 +1,12 @@
-// function add(num1, num2) {
-//     return num1 + num2;
-// }
-// console.log(add(200,10));
-// const num = Number(prompt("What's your favorite number:?"));
+let hasDriverLicense = false;
+let isSober = false;
+let driver = prompt("Do you have a Valid Driver's License? (y or n): ");
+if (driver.toLowerCase() === "y") hasDriverLicense = true;
+let drink = "";
 
-// if (num === 1) console.log(num);
-// console.log(typeof num);
-// if (num !== 1) console.log(num + " num is not 1");
-// else console.log("num is 1");
-// let hasDriverLicense = false;
-// let isSober = false;
-// hasDriverLicense = Boolean(prompt("Do you have a Valid Driver's License:?"));
-// isSober = Boolean(prompt("Are you sober:?"));
-
-// let hasDriverLicense = false;
-// let isSober = false;
-let hasDriverLicense = prompt(
-  "Do you have a Valid Driver's License? (y or n): "
-);
-//if (driver === "y") hasDriverLicense = true;
-// if (hasDriverLicense) {
-hasDriverLicense = hasDriverLicense.toLowerCase();
-
-if (hasDriverLicense === "y") {
-  isSober = prompt("Are you sober? (y or n): ");
-  if (isSober === "y" || isSober === "Y") console.log("Can drive");
+if (hasDriverLicense) {
+  drink = prompt("Are you sober? (y or n): ");
+  if (drink.toLowerCase() === "y") isSober = true;
+  if (isSober) console.log("Can drive");
   else console.log("Cannot drive");
 } else console.log("Cannot drive");
