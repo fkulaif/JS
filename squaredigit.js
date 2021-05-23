4eb2a977e4a720fe0d4994c7181eef2cf13d1fb3
Pingosoft <fernandokulaif@gmail.com>
2021-05-23 04:00:49 -0400

    squaredigit.js w/ split/map/join

--- squaredigit.js
+++ squaredigit.js
@@ -1,12 +1,11 @@
+console.log(squareDigits(331));
 function squareDigits(num) {
-  num = num.toString();
-  let arrNum = Array.from(num);
-  var res = [];
-  var i;
-  for (i = 0; i < arrNum.length; i++) {
-    res[i] = arrNum[i] ** 2;
-  }
-  res = res.toString();
-  res = res.replaceAll(",", "");
-  return Number(res);
+  return Number(
+    ("" + num)
+      .split("")
+      .map(function (val) {
+        return val ** 2;
+      })
+      .join("")
+  );
 }
